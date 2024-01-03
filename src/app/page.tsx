4d53105b6e7d02192ee58c86130c4eae37e79570
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2024-01-03 10:46:21 +0530

    chore: nuke useState and use zustand

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,32 +7,33 @@ import { Button } from "@nextui-org/react";
 import { Image } from "@nextui-org/react";
 
 import { Dispatch, SetStateAction } from "react";
-
-type dispatch_bool = Dispatch<SetStateAction<boolean>>;
-type dispatch_str = Dispatch<SetStateAction<string>>;
+import { stateOptic } from "./store";
 
 export default function Page() {
   // states
-  const [loggerInUser, setLoggedInUser] = useState<ID | null>(null);
-  const [email, setemail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
   const [name, setName] = useState<string>("");
-
   const [isLoginLoading, setisLoginLoading] = useState<boolean>(false);
 
+  const u_name = stateOptic((state) => (state.u_name))
+  const u_email = stateOptic((state) => (state.u_email))
+  const u_pass = stateOptic((state) => (state.u_pass))
+  const updateUserName = stateOptic((state) => (state.update_name))
+  const updateUserEmail = stateOptic((state) => (state.update_email))
+  const updateUserPass = stateOptic((state) => (state.update_pass))
+
   const register = async () => {
-    console.log(email, password, name)
-    await account.create(ID.unique(), email, password, name)
+    console.log(u_email, u_pass, name)
+    await account.create(ID.unique(), u_email, u_pass, name)
   }
   const login = async (email: string, password: string) => {
     setisLoginLoading(true)
     const session = await account.createEmailSession(email, password)
-    setLoggedInUser(await account.get());
+    updateUserName(await account.get())
     setisLoginLoading(await account.get()? false : true);
   }
   const logout = async () => {
     await account.deleteSession("current");
-    setLoggedInUser(null)
+    updateUserName(null)
   }
 
   function crm_logout() { }
@@ -42,10 +43,10 @@ export default function Page() {
     console.log("fk")
   }
   
-  if (loggerInUser) {
+  if (u_name) {
     return (
       <main className="flex flex-col min-h-screen justify-center items-center">
-        <p>Logged in as {loggerInUser.name != null}</p>
+        <p>Logged in as {u_name?.name}</p>
         <button className={`${Buttons.basic}`} onClick={logout} type="button">Sign out</button>
       </main>
     )
@@ -71,18 +72,18 @@ export default function Page() {
             <input type="email"
                 className={`${Input.basic} m-2`}
                 placeholder="Email Address"
-                value={email}
-                onChange={(e) => {setemail(e.target.value)}}
+                value={u_email}
+                onChange={(e) => {updateUserEmail(e.target.value)}}
             />
             <input type="password"
                 className={`${Input.basic} m-2`}
                 placeholder="Password"
-                value={password}
-                onChange={(e) => {setPassword(e.target.value)}}
+                value={u_pass}
+                onChange={(e) => {updateUserPass(e.target.value)}}
             />
             <div className="flex flex-row justify-center m-6">
               <Button className={`${Buttons.basic}`} onClick={register}>Register</Button>
-              <Button className={`${Buttons.basic}`} isLoading={isLoginLoading} onClick={() => { login(email, password) }}>Login</Button>
+              <Button className={`${Buttons.basic}`} isLoading={isLoginLoading} onClick={() => { login(u_email, u_pass) }}>Login</Button>
             </div>
         </div>
       </div>
